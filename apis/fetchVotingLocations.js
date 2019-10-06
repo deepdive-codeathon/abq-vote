4d2f7af8dbfd1a7eb076e397356f19e3721a8232
Joseph Peterson <jscpeterson@gmail.com>
2019-10-05 20:01:10 -0600

    MapScreen changed to class based component

---
+++ apis/fetchVotingLocations.js
@@ -0,0 +1,18 @@
+// import { Permissions, Notifications, Constants } from 'expo';
+// import { Constants } from 'expo-constants';
+
+// TODO Move to querys.js and mutations.js
+
+// TODO Move this to .env
+const PUSH_ENDPOINT = 'https://coagisweb.cabq.gov/arcgis/rest/services/public/Voting2019/MapServer/3/query?where=&text=&objectIds=&time=&geometry=35.08673368614533%2C+-106.56775471868217+&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson';
+
+export default (async function fetchVotingLocations() {
+
+  return fetch(PUSH_ENDPOINT, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+   })
+});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import MapView from 'react-native-maps';
+import fetchVotingLocations from '../apis/fetchVotingLocations'
 
-export default function MapScreen() {
+export default class MapScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewingRegion: {latitude: 35.0844,
+                longitude: -106.6504,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,},
+            locations: []
+        }
+    }
+
+    componentDidMount() {
+        fetchVotingLocations().then(response => response.json()
+        .then(jsonData => this.setState({jsonData}))
+        , fail=>console.log(fail))
+    }
+
+    render() {
     return (
-        <View style={styles.container}>
             <MapView style={styles.map}
-                initialRegion={{
-                    latitude: 35.0844,
-                    longitude: -106.6504,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
-        </View>
-
-    );
+            initialRegion={this.state.viewingRegion}>
+            {/* {this.state.locations.array.forEach(element => {
+                console.log(element)
+            })} */}
+            {/* {locations.array.map(marker => {
+                    console.log(marker)
+                    // <Marker
+                    //     coordinate={{element}["geometry"]}
+                    //     title={{element}["attributes"]["name"]}
+                    // />
+                })} */}
+            </MapView>
+        );
+    }
 }
 
 MapScreen.navigationOptions = {
